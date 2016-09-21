57c4416bfb06a77a5568f30129a351f48fe4cbc3
王志成 <wangzhicheng@tongxinyiliao.com>
2016-09-21 15:04:12 +0800

    add readme move migrations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# flea
+自制爬虫
+  可用 activerecord
+  可下载文件
--- migrations/create_orgs.rb
+++ migrations/create_orgs.rb
@@ -1,14 +1,3 @@
-require 'active_record'
-
-ActiveRecord::Base.establish_connection(
-    :adapter => "mysql2",
-    :database  => "data_acquisition_development",
-    :username  => "root",
-    :password  => "hdwzc",
-    :socket  => "/tmp/mysql.sock"
-  )
-
-
 ActiveRecord::Schema.define do
   drop_table :orgs if table_exists? :orgs
   create_table :orgs do |table|
